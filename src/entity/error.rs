91b7266719996b2caf49e82ecfffc0a5b2dfe2fd
Lakshyyaa <lakshh113@gmail.com>
2025-07-24 21:35:48 +0530

    refactor: remove entity module completely, use es_entity

--- src/entity/error.rs
+++ src/entity/error.rs
@@ -1,9 +1,9 @@
-use thiserror::Error;
+// #[derive(Error, Debug)]
+// pub enum EntityError {
+//     #[error("EntityError - UninitializedFieldError: {0}")]
+//     UninitializedFieldError(#[from] derive_builder::UninitializedFieldError),
+//     #[error("EntityError - LoadEvent: {0}")]
+//     LoadEvent(#[from] serde_json::Error),
+// }
 
-#[derive(Error, Debug)]
-pub enum EntityError {
-    #[error("EntityError - UninitializedFieldError: {0}")]
-    UninitializedFieldError(#[from] derive_builder::UninitializedFieldError),
-    #[error("EntityError - LoadEvent: {0}")]
-    LoadEvent(#[from] serde_json::Error),
-}
+// Instead of adding the from_es_entity_error, do we just add the 1/2 error enums from above in each?
